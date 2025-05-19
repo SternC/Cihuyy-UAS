a9270f6157533191c2a8c2aae0db176ca987b0b0
Willyapee <your_email@example.com>
2025-05-19 21:18:49 +0700

    main.jsx gapapa kok gais

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import MenuComponent from './pages/start.jsx'
-import './main.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
